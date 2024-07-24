ae5d18d981bbb855588398b4c8391408b38902f4
jungeun <angal2310@tukorea.ac.kr>
2024-07-25 00:45:42 +0900

    feat : 모니터링 세팅
    배포 환경으로 이동

--- app.js
+++ app.js
@@ -11,6 +11,26 @@ const cors = require('cors'); // cors 패키지 로드
 const http = require('http'); // http 패키지 로드
 const path = require('path'); // path 패키지 로드
 const app = express(); // express 애플리케이션 생성
+const { register, httpRequestDurationMicroseconds, requestCount } = require('./prometheus/metrics');
+
+app.use(cors()); // CORS 미들웨어 추가
+app.use(express.json());  // Middleware 설정 
+
+// 메트릭 수집용 미들웨어 설정
+app.use((req, res, next) => {
+  const end = httpRequestDurationMicroseconds.startTimer();
+  res.on('finish', () => {
+    end({ method : req.method, route: req.route ? req.route.path : req.path, code : res.statusCode });
+    requestCount.inc({ method: req.method, route: req.route ? req.route.path : req.path, code: res.statusCode });
+  });
+  next();
+});
+
+// 프로메테우스 엔드포인트
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', register.contentType);
+  res.end(await register.metrics());
+});
 
 // Socket.io 설정
 const server = http.createServer(app);
@@ -23,9 +43,6 @@ const io = new Server(server, {
   }
 });
 const chatSocket = require('./src/socket/chat'); 
-
-app.use(cors()); // CORS 미들웨어 추가
-app.use(express.json());  // Middleware 설정 
 
 // const mongoURI = process.env.MONGO_LOCAL_URL; // 로컬로 실행시
 const mongoURI = process.env.MONGODB_DOCKER_URL; 
